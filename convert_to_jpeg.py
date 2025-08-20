c65b9487adbc4b5f60f48d43566012dcc5d3cfc3
Caleb Chow <howanchow2080@gmail.com>
2025-08-20 11:50:06 -0500

    Enhance: convert all images in folder automatically instead of typing manually

--- convert_to_jpeg.py
+++ convert_to_jpeg.py
@@ -10,24 +10,33 @@ import pillow_heif
 #register HEIC/HEIF support with pillow
 pillow_heif.register_heif_opener()
 
-#loop through all image files
-for input in sys.argv[1:]:
-    #split the file name into the name and the extension
-    name, ext = os.path.splitext(input)
+#the folder that has images wanting to convert (change "." to the actual path)
+input_folder = "."
 
-    #set output as name + jpg
-    output = name + ".jpg"
+#go through every file in the image folder
+for filename in os.listdir(input_folder):
+    #set input as the path to the file
+    input = os.path.join(input_folder, filename)
 
-    #check if input is different than output
-    if input != output:
-        #try and except
-        try:
-            #set im as the opened image and close the image
-            with Image.open(input) as im:
-                im.save(output)
+    #separate the extension from input
+    f, e = os.path.splitext(input)
 
-                #print message
-                print(f"converted {input} > {output}")
-        except OSError:
-            #print cannot convert
-            print(f"cannot convert {input}")
+    #set output's extension as .jpg
+    output = f + ".jpg"
+
+    #skip through all .jpg
+    if e.lower() == ".jpg":
+        continue
+
+    #try and except OSError
+    try:
+        #set im as the opened image and close it afterwards
+        with Image.open(input) as im:
+            im.save(output)
+
+            #print message
+            print(f"Convert {input} > {output}")
+
+    except OSError:
+        #print message
+        print(f"Cannot convert {input}")
